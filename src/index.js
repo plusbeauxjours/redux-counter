3c8746fc193327fcf6c17444c527dcb92a249c35
plusbeauxjours <plusbeauxjours@gmail.com>
2017-12-15 00:54:46 +0900

    Create Redux Store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+// redux
+import { createStore } from 'redux'
+import reducers from './reducers';
+
+// store
+const store = createStore(reducers);
+
 ReactDOM.render(
     <App />, 
     document.getElementById('root')
