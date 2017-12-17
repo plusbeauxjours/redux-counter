a9ce62dd91e776e44d6556eaac36eab8b3567f7f
plusbeauxjours <plusbeauxjours@gmail.com>
2017-12-17 23:14:47 +0900

    Done Counter Container

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App";
+import "./index.css";
 
-// redux
-import { createStore } from 'redux'
-import reducers from './reducers';
+// Redux 관련 불러오기
+import { createStore } from "redux";
+import reducers from "./reducers";
+import { Provider } from "react-redux";
 
-// store
+// 스토어 생성
 const store = createStore(reducers);
 
 ReactDOM.render(
-    <App />, 
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
